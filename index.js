a2708af61ea6463be67671bacab98a973ea84af9
DEDSEC-WEAPON\minh <mnguyenngoc69@gmail.com>
2024-10-30 18:28:15 +0700

    cau hinh mvc

--- index.js
+++ index.js
@@ -6,17 +6,9 @@ const port = 3000;
 const database = require("./config/database");
 database.connect();
 
-const Task = require("./model/task.model")
+const routeClient = require("./routes/client/index.route");
 
-app.get("/tasks/detail/:id", async (req, res) => {
-  const id = req.params.id;
-  const task = await Task.find({
-    _id: id,
-    deleted: false,
-  });
-
-  res.json(task);
-});
+routeClient(app);
 
 app.listen(port, () => {
   console.log(`App listening on port ${port}`);
